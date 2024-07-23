481bfde6ec8795a719b6637c3486dbb07d25f3f4
Mehul Singh <143124065+csemehul@users.noreply.github.com>
2024-07-23 21:51:23 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  imageUrl: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
 
-const Product = mongoose.model('Product', ProductSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', ProductSchema);
